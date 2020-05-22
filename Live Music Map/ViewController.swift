4b87f1dd8832b2f95fb96c04fe40099050940b52
cozenakai <masaki2970@icloud.com>
2020-05-23 02:30:01 +0900

    Initial Commit

---
+++ Live Music Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Live Music Map
+//
+//  Created by Masaki Chonan on 2020/05/23.
+//  Copyright © 2020 Masaki Chonan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Live Music MapTests/Live_Music_MapTests.swift
@@ -0,0 +1,34 @@
+//
+//  Live_Music_MapTests.swift
+//  Live Music MapTests
+//
+//  Created by Masaki Chonan on 2020/05/23.
+//  Copyright © 2020 Masaki Chonan. All rights reserved.
+//
+
+import XCTest
+@testable import Live_Music_Map
+
+class Live_Music_MapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
