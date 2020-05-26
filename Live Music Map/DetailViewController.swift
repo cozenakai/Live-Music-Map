e6a270ee68a7bfdff92f5900e11aa91948c9be55
cozenakai <masaki2970@icloud.com>
2020-05-27 01:03:47 +0900

    地図の表示と遷移まで！！

---
+++ Live Music Map/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Live Music Map
+//
+//  Created by Masaki Chonan on 2020/05/27.
+//  Copyright © 2020 Masaki Chonan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Live Music Map/ViewController.swift
+++ Live Music Map/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate{
+    
+    
+    @IBOutlet var mapView:MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude:35.658517, longitude: 139.70133399999997), latitudinalMeters: 500, longitudinalMeters: 500)
+        mapView.setRegion(region, animated: false)
+        
+        var pin = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 35.6581, longitude: 139.7017))
+        
+        mapView.addAnnotation(pin)
+        
     }
 
-
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        performSegue(withIdentifier: "tapped", sender: self)
+    }
 }
 
